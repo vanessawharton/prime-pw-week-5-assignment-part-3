3325f26f0eb4f41f490160a50353f337093520c3
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-10-23 21:34:55 -0500

    Adding addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,5 +3,10 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection(title, artist, yearPublished){
+    newAddition.title = title;
+    newAddition.artist = artist;
+    newAddition.yearPublished = yearPublished;
     return newAddition;
 }
+
+console.log(newAddition);
