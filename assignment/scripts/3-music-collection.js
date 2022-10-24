4f37dcad430a539bff8ba2d05a2ee7b87435a676
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-10-23 23:20:07 -0500

    Added showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,14 @@
 console.log('***** Music Collection *****')
 
+//Create a variable collection that starts as an empty array.
+
 let collection = [];
+
+//Add a function named addToCollection. This function should:
+//Take in the album's title, artist, yearPublished as input parameters
+//Create a new object having the above properties
+//Add the new object to the end of the collection array
+//Return the newly created object
 
 function addToCollection(title, artist, yearPublished){
     const newAddition = title + artist + yearPublished;
@@ -10,6 +18,12 @@ function addToCollection(title, artist, yearPublished){
     collection.push(newAddition);
     return newAddition;
 }
+
+//Test the addToCollection function:
+//Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years.
+//Console.log each album as added using the returned value.
+//After all are added, console.log the collection array.
+
 
 addToCollection('In Rainbows', 'Radiohead', 2007);
 console.log('First Album:', collection[collection.length -1]);
@@ -33,3 +47,26 @@ addToCollection('Wasting Light', 'Foo Fighters', 2011);
 console.log('Tenth Album:', collection[collection.length -1]);
 
 console.log(collection);
+
+//Add a function named showCollection. This function should:
+//Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
+//Console.log the number of items in the array.
+//Loop over the array and console.log each album's information formatted like: TITLE by ARTIST, published in YEAR.
+
+function showCollection(collectionArray){
+    for (i=0; i<collectionArray.length; i++){
+        console.log(TITLE, " by ", ARTIST, ", published in ", YEARPUBLISHED);
+    }
+    console.log(collectionArray.length);
+}
+
+//Test the showCollection function.
+
+//Add a function named findByArtist. This function should:
+//Take in artist (a string) parameter
+//Create an array to hold any results, empty to start
+//Loop through the collection and add any objects with a matching artist to the array.
+//Return the array with the matching results. If no results are found, return an empty array.
+
+//Test the findByArtist function. Make sure to test with an artist you know is in the collection, as well as an artist
+//you know is not in your collection. Check that for artists with multiple matches, all are found.
