2018c83de2e6ba8666342cbd55480325d27cc74d
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-10-23 21:47:01 -0500

    Adding console.logs for albums added to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,12 +12,22 @@ function addToCollection(title, artist, yearPublished){
 }
 
 addToCollection('In Rainbows', 'Radiohead', 2007);
+console.log('First Album:', collection.push);
 addToCollection('Everything Now', 'Arcade Fire', 2017);
+console.log('Second Album:', collection.push);
 addToCollection('My Cherie Amour', 'Stevie Wonder', 1969);
+console.log('Third Album:', collection.push);
 addToCollection('Bon Iver', 'Bon Iver', 2011);
+console.log('Fourth Album:', collection.push);
 addToCollection('Rumors', 'Fleetwood Mac', 1977);
+console.log('Fifth Album:', collection.push);
 addToCollection('Neon Bible', 'Arcade Fire', 2007);
+console.log('Sixth Album:', collection.push);
 addToCollection('Let It Bleed', 'Rolling Stones', 1969);
+console.log('Seventh Album:', collection.push);
 addToCollection('Led Zeppelin II', 'Led Zeppelin', 1969);
+console.log('Eighth Album:', collection.push);
 addToCollection('American Teen', 'Khalid', 2017);
+console.log('Ninth Album:', collection.push);
 addToCollection('Wasting Light', 'Foo Fighters', 2011);
+console.log('Tenth Album:', collection.push);
