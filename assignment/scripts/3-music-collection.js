2e6ce7976c5327fc15a9521678c776b3e10b9f0e
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-10-23 21:44:05 -0500

    Adding albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,3 +12,12 @@ function addToCollection(title, artist, yearPublished){
 }
 
 addToCollection('In Rainbows', 'Radiohead', 2007);
+addToCollection('Everything Now', 'Arcade Fire', 2017);
+addToCollection('My Cherie Amour', 'Stevie Wonder', 1969);
+addToCollection('Bon Iver', 'Bon Iver', 2011);
+addToCollection('Rumors', 'Fleetwood Mac', 1977);
+addToCollection('Neon Bible', 'Arcade Fire', 2007);
+addToCollection('Let It Bleed', 'Rolling Stones', 1969);
+addToCollection('Led Zeppelin II', 'Led Zeppelin', 1969);
+addToCollection('American Teen', 'Khalid', 2017);
+addToCollection('Wasting Light', 'Foo Fighters', 2011);
